84284ef5b32f88b61c7fc3afdcff934900c3cf31
moncada jeremy <jeremy.moncada@etu.unice.fr>
2024-03-20 11:41:13 +0100

    push

---
+++ app/src/main/java/filrouge/groupei/boredealsappandroid/CodeActivity.java
@@ -0,0 +1,15 @@
+package filrouge.groupei.boredealsappandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code);
+
+    }
+}
--- app/src/main/java/filrouge/groupei/boredealsappandroid/MainActivity.java
+++ app/src/main/java/filrouge/groupei/boredealsappandroid/MainActivity.java
@@ -2,7 +2,10 @@ package filrouge.groupei.boredealsappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonJEnProfite = findViewById(R.id.button_j_en_profite);
+
+        buttonJEnProfite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CodeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
