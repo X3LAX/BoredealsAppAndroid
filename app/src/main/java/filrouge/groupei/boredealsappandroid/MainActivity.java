787fb5d8b33669f8d1ea833e40869609e82dc911
moncada jeremy <jeremy.moncada@etu.unice.fr>
2024-03-25 11:41:54 +0100

    push

--- app/src/main/java/filrouge/groupei/boredealsappandroid/MainActivity.java
+++ app/src/main/java/filrouge/groupei/boredealsappandroid/MainActivity.java
@@ -2,50 +2,52 @@ package filrouge.groupei.boredealsappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private StoreAdapter storeAdapter;
     private List<Store> storeList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false));
-
 
         storeList = generateSampleData();
         storeAdapter = new StoreAdapter(storeList);
         recyclerView.setAdapter(storeAdapter);
 
         SeekBar seekBar = findViewById(R.id.seekBar);
+        TextView percentageTextView = findViewById(R.id.percentageTextView);
+
+        seekBar.setMax(100);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                List<Store> filteredStores = new ArrayList<>();
-                for (Store store : storeList) {
-                    if (store.getDiscountPercentage() >= progress) {
-                        filteredStores.add(store);
+                if (progress >= 0 && progress < getResources().getStringArray(R.array.discount_percentages).length) {
+                    String percentage = getResources().getStringArray(R.array.discount_percentages)[progress];
+                    percentageTextView.setText(percentage);
+                    List<Store> filteredStores = new ArrayList<>();
+                    for (Store store : storeList) {
+                        if (store.getDiscountPercentage() >= parsePercentage(percentage)) {
+                            filteredStores.add(store);
+                        }
                     }
+                    storeAdapter.setData(filteredStores);
                 }
-                storeAdapter.setData(filteredStores);
             }
 
             @Override
@@ -58,7 +60,10 @@ public class MainActivity extends AppCompatActivity {
                 // Ne rien faire
             }
         });
+    }
 
+    private int parsePercentage(String percentageString) {
+        return Integer.parseInt(percentageString.replace("%", ""));
     }
 
     private List<Store> generateSampleData() {
@@ -66,14 +71,16 @@ public class MainActivity extends AppCompatActivity {
 
         String[] names = getResources().getStringArray(R.array.characters);
         String[] descriptions = getResources().getStringArray(R.array.description);
-        Random random = new Random();
+        String[] percentages = getResources().getStringArray(R.array.discount_percentages);
+
+        if (names.length != descriptions.length || names.length != percentages.length) {
+            return stores;
+        }
 
         for (int i = 0; i < names.length; i++) {
-            int discountPercentage = random.nextInt(41);
-            stores.add(new Store(names[i], descriptions[i], discountPercentage));
+            stores.add(new Store(names[i], descriptions[i], parsePercentage(percentages[i])));
         }
 
         return stores;
     }
-
 }
