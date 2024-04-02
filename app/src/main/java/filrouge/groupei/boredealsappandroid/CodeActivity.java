b8f5a982b6591d452496f33c79de8a7dbc7c7a21
moncada jeremy <jeremy.moncada@etu.unice.fr>
2024-04-03 00:33:33 +0200

    push

--- app/src/main/java/filrouge/groupei/boredealsappandroid/CodeActivity.java
+++ app/src/main/java/filrouge/groupei/boredealsappandroid/CodeActivity.java
@@ -4,16 +4,20 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -22,6 +26,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
@@ -43,6 +51,9 @@ public class CodeActivity extends AppCompatActivity {
 
         TextView textDiscount = findViewById(R.id.textDiscount);
         textDiscount.setText(store.getDescription());
+
+        String websiteLink = store.getLink();
+        Bitmap qrCodeBitmap = generateQRCode(websiteLink, 500, 500);
 
         //String logoUrl = "https://github.com/X3LAX/BoredealsAppAndroidRessources/blob/main/logos/apple_logo.png?raw=true";//+ store.getName() + "_logo.png?raw=true";
 
@@ -208,6 +219,45 @@ public class CodeActivity extends AppCompatActivity {
             }
         });
         animatorSet.start();
+    }
+
+    private Bitmap generateQRCode(String text, int width, int height) {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private void showQRCodeDialog(Bitmap qrCodeBitmap) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.activity_qrcode, null);
+        builder.setView(dialogView);
+
+        ImageView imageViewQRCode = dialogView.findViewById(R.id.imageQRCode);
+        imageViewQRCode.setImageBitmap(qrCodeBitmap);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        Button buttonClose = dialogView.findViewById(R.id.buttonClose);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
 }
 
