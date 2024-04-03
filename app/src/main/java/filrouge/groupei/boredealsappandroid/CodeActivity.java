81dde0f505dc7f34ca030f68900f1cee9c3eefbe
moncada jeremy <jeremy.moncada@etu.unice.fr>
2024-04-03 20:49:12 +0200

    push

--- app/src/main/java/filrouge/groupei/boredealsappandroid/CodeActivity.java
+++ app/src/main/java/filrouge/groupei/boredealsappandroid/CodeActivity.java
@@ -18,18 +18,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
@@ -37,28 +40,18 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
-import android.os.Bundle;
-import android.widget.RatingBar;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-
 public class CodeActivity extends AppCompatActivity {
 
-    private RatingBar ratingBar;
     private DatabaseReference databaseReference;
+    private String userId; // Assuming userId is available in your app
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code);
 
-        // Utilisation de la classe store parcelée pour afficher les informations dans l'activité CodeActivity
         Store store = getIntent().getParcelableExtra("selectedStore");
-
         if (store == null) return;
-
 
         TextView textRecommendation = findViewById(R.id.textRecommendation);
         textRecommendation.setText(store.getName());
@@ -69,103 +62,24 @@ public class CodeActivity extends AppCompatActivity {
         String websiteLink = store.getLink();
         Bitmap qrCodeBitmap = generateQRCode(websiteLink, 500, 500);
 
-
         String baseUrl = "https://github.com/X3LAX/BoredealsAppAndroidRessources/blob/main/logos/";
         String logoUrl = baseUrl + store.getName().toLowerCase() + "_logo.png?raw=true";
 
-
         ImageView imageLogo = findViewById(R.id.imageLogo);
         Picasso.get()
-            .load(logoUrl)
-            .error(R.drawable.bell)
-            .into(new Target() {
-                @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    imageLogo.setImageBitmap(bitmap);
-                    Log.d("Picasso", "Image loaded successfully");
-                }
-
-                @Override
-                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-                    imageLogo.setImageDrawable(errorDrawable);
-                    Log.e("Picasso", "Error loading image", e);
-                }
-
-                @Override
-                public void onPrepareLoad(Drawable placeHolderDrawable) {
-                }
-            });
-
+                .load(logoUrl)
+                .error(R.drawable.bell)
+                .into(imageLogo);
 
         RatingBar ratingBar = findViewById(R.id.ratingBar);
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(CodeActivity.this, String.valueOf(rating), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-
         CardView cardPromoCode = findViewById(R.id.cardPromoCode);
-
-        float initialScale = 1f;
-
-        float finalScale = 1.6f;
-
-        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(
-                cardPromoCode,
-                View.SCALE_X,
-                initialScale,
-                finalScale
-        );
-
-        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(
-                cardPromoCode,
-                View.SCALE_Y,
-                initialScale,
-                finalScale
-        );
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(scaleXAnimator, scaleYAnimator);
-
-        animatorSet.setDuration(300);
-
-        animatorSet.start();
-
-        animatorSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                ObjectAnimator scaleXResetAnimator = ObjectAnimator.ofFloat(
-                        cardPromoCode,
-                        View.SCALE_X,
-                        finalScale,
-                        initialScale
-                );
-
-                ObjectAnimator scaleYResetAnimator = ObjectAnimator.ofFloat(
-                        cardPromoCode,
-                        View.SCALE_Y,
-                        finalScale,
-                        initialScale
-                );
-
-                AnimatorSet resetAnimatorSet = new AnimatorSet();
-                resetAnimatorSet.playTogether(scaleXResetAnimator, scaleYResetAnimator);
-
-                resetAnimatorSet.setDuration(300);
-
-                resetAnimatorSet.setStartDelay(300);
-                resetAnimatorSet.start();
-            }
-        });
+        animateCard(cardPromoCode);
 
         TextView textPromoCode = findViewById(R.id.textPromoCode);
         textPromoCode.setText(store.getPromocode());
-        TextView textCopier = findViewById(R.id.textCopier);
 
+        TextView textCopier = findViewById(R.id.textCopier);
         textCopier.setOnClickListener(v -> {
             String codePromo = store.getPromocode();
             ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
@@ -179,81 +93,48 @@ public class CodeActivity extends AppCompatActivity {
             startActivity(browserIntent);
         });
 
-
-        animateCard(cardPromoCode);
-
         Button buttonGenerateQRCode = findViewById(R.id.boutonQrCode);
-        buttonGenerateQRCode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showQRCodeDialog(qrCodeBitmap);
-            }
-        });
+        buttonGenerateQRCode.setOnClickListener(v -> showQRCodeDialog(qrCodeBitmap));
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("ratings");
+        // Récupérer l'ID utilisateur (à adapter à votre logique)
+        userId = getUserId(); // Méthode à implémenter
 
+        // Initialiser la référence de la base de données
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
-
-        if (store == null) return;
-
-        float savedRating = getSavedRatingFromPreferences();
-
+        // Récupérer et afficher la note sauvegardée localement
+        float savedRating = getSavedRatingFromPreferences(String.valueOf(store.getId()));
         ratingBar.setRating(savedRating);
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                saveRatingToFirebase(rating);
-
-                saveRatingLocally(rating);
-
-                Toast.makeText(CodeActivity.this, "Évaluation enregistrée avec succès", Toast.LENGTH_SHORT).show();
-            }
+        // Enregistrer la note lorsqu'elle change et afficher un message Toast
+        ratingBar.setOnRatingBarChangeListener((ratingBar1, rating, fromUser) -> {
+            saveRatingToFirebase(rating, String.valueOf(store.getId()));
+            saveRatingLocally(rating, String.valueOf(store.getId()));
+            Toast.makeText(CodeActivity.this, "Évaluation enregistrée avec succès", Toast.LENGTH_SHORT).show();
         });
     }
 
-    private void saveRatingLocally(float rating) {
+    // Méthode pour récupérer l'ID utilisateur (à adapter à votre logique)
+    private String getUserId() {
+        // Implémentez ici la logique pour récupérer l'ID utilisateur
+        return "user123"; // Exemple statique, remplacez-le par votre propre logique
+    }
+
+    private float getSavedRatingFromPreferences(String storeId) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        return sharedPreferences.getFloat(storeId, 0.0f);
+    }
+
+
+    private void saveRatingLocally(float rating, String storeId) {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putFloat("rating", rating);
+        editor.putFloat(storeId, rating);
         editor.apply();
     }
 
-    private void saveRatingToFirebase(float rating) {
-        databaseReference.push().setValue(rating);
-    }
-
-    private float getSavedRatingFromPreferences() {
-        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        return sharedPreferences.getFloat("rating", 0.0f);
-    }
-
-    private void animateCard(CardView cardView) {
-        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(cardView, View.SCALE_X, 0.8f, 1.0f);
-        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(cardView, View.SCALE_Y, 0.8f, 1.0f);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(scaleXAnimator, scaleYAnimator);
-        animatorSet.setDuration(500);
-        animatorSet.start();
-    }
-
-    private void explodeText(TextView textView) {
-        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(textView, View.SCALE_X, 1.0f, 1.5f);
-        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(textView, View.SCALE_Y, 1.0f, 1.5f);
-
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(scaleXAnimator, scaleYAnimator);
-        animatorSet.setDuration(600);
-        animatorSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                textView.setScaleX(1.0f);
-                textView.setScaleY(1.0f);
-            }
-        });
-        animatorSet.start();
+    private void saveRatingToFirebase(float rating, String storeId) {
+        databaseReference.child("ratings").child(userId).child(storeId).setValue(rating);
     }
 
     private Bitmap generateQRCode(String text, int width, int height) {
@@ -286,14 +167,16 @@ public class CodeActivity extends AppCompatActivity {
         dialog.show();
 
         Button buttonClose = dialogView.findViewById(R.id.buttonClose);
-        buttonClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
+        buttonClose.setOnClickListener(v -> dialog.dismiss());
+    }
+
+    private void animateCard(CardView cardView) {
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(cardView, View.SCALE_X, 0.8f, 1.0f);
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(cardView, View.SCALE_Y, 0.8f, 1.0f);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(scaleXAnimator, scaleYAnimator);
+        animatorSet.setDuration(500);
+        animatorSet.start();
     }
 }
-
-
-
